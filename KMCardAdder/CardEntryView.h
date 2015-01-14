b528c3b7ca687423caff812a9ebbcd806c402553
Kabir Mahal <kabirmahal@gmail.com>
2015-01-13 23:54:56 -0800

    added assets

---
+++ KMCardAdder/CardEntryView.h
@@ -0,0 +1,13 @@
+//
+//  CardEntryView.h
+//  KMCardAdder
+//
+//  Created by Kabir Mahal on 1/13/15.
+//  Copyright (c) 2015 KM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardEntryView : UIView
+
+@end
---
+++ KMCardAdder/CardEntryView.m
@@ -0,0 +1,30 @@
+//
+//  CardEntryView.m
+//  KMCardAdder
+//
+//  Created by Kabir Mahal on 1/13/15.
+//  Copyright (c) 2015 KM. All rights reserved.
+//
+
+#import "CardEntryView.h"
+
+@implementation CardEntryView
+
+
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    
+    self = [super initWithFrame:frame];
+    
+    if (self){
+        
+    }
+    
+    return self;
+    
+}
+
+
+
+
+@end
